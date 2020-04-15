a2bfcc678eb3f3a4ab5d30c76df1e791ecd0d9bd
kay-raph <kayraph3d@gmail.com>
2020-04-16 01:56:30 +0300

    added cms

--- js/script.js
+++ js/script.js
@@ -2,42 +2,36 @@
  $(document).ready(function () {
     $(".exp p1").hide();
    $(".exp").click(function () {
-      $(".exp p1").toggle(1000);
-      $(".exp #mg1").slideDown(1000).show(1000);
+       $(".exp p1").css("color", "black").toggle(1000);
+      $(".exp #mg1").toggle(1000).show(1000);
     });
-// $(document).ready(function(){
-//     $("img").click(function(){
-//       $("img").slideDown(200).hide(1000);
-//       $("p1").slideUp(200).show(1000);
-//       $("img").toggle();
-//   });
 
     $(".dev p2").hide();
     $(".dev").click(function () {
-        $(".dev p2").toggle(1000);
-        $(".dev #mg2").slideDown(1000).show(1000);;
+        $(".dev p2").css("color", "black").toggle(1000);
+        $(".dev #mg2").toggle(1000).show(1000);;
     });
 
 
     $(".grap p3").hide();
     $(".grap").click(function () {
-        $(".grap p3").toggle(1000);
-        $(".grap #mg3").slideDown(1000).show(1000);;
+        $(".grap p3").css("color", "black").toggle(1000);
+        $(".grap #mg3").toggle(1000).show(1000);;
     });
     $(".ex p4").hide();
     $(".ex").click(function () {
-        $(".ex p4").toggle(1000);
-        $(".ex #mg4").slideDown(1000).show(1000);;
+        $(".ex p4").css("color", "black").toggle(1000);
+        $(".ex #mg4").toggle(1000).show(1000);;
     });
     $(".dj p5").hide();
     $(".dj").click(function () {
-        $(".dj p5").toggle(1000);
-        $(".dj #mg5").slideDown(1000).show(1000);;
+        $(".dj p5").css("color", "black").toggle(1000);
+        $(".dj #mg5").toggle(1000).show(1000);;
     });
     $(".loops p6").hide();
     $(".loops").click(function () {
-        $(".loops p6").toggle(1000);
-        $(".loops #mg6").slideDown(1000).show(1000);;
+        $(".loops p6").css("color", "black").toggle(1000);
+        $(".loops #mg6").toggle(1000).show(1000);;
     });
     $(".hobby .int").hide();
     $(".hobby").click(function () {
