6e9cb096502a1d6e5615964d729cba9ce14876f1
kay-raph <kayraph3d@gmail.com>
2020-05-13 18:06:45 +0300

    added time in footer

--- js/script.js
+++ js/script.js
@@ -23,15 +23,20 @@
         $(".ex p4").css("color", "black").toggle(1000);
         $(".ex #mg4").toggle(1000).show(1000);;
     });
-    $(".dj p5").hide();
-    $(".dj").click(function () {
-        $(".dj p5").css("color", "black").toggle(1000);
-        $(".dj #mg5").toggle(1000).show(1000);;
+    $(".exe p5").hide();
+    $(".exe").click(function () {
+        $(".exe p5").css("color", "black").toggle(1000);
+        $(".exe #mg5").toggle(1000).show(1000);;
     });
-    $(".loops p6").hide();
+    $(".dj p6").hide();
+    $(".dj").click(function () {
+        $(".dj p6").css("color", "black").toggle(1000);
+        $(".dj #mg6").toggle(1000).show(1000);;
+    });
+    $(".loops p7").hide();
     $(".loops").click(function () {
-        $(".loops p6").css("color", "black").toggle(1000);
-        $(".loops #mg6").toggle(1000).show(1000);;
+        $(".loops p7").css("color", "black").toggle(1000);
+        $(".loops #mg7").toggle(1000).show(1000);;
     });
     $(".hobby .int").hide();
     $(".hobby").click(function () {
@@ -39,4 +44,3 @@
         $(".hobby").slideUp(1000).show(1000);;
     });
 });
-
